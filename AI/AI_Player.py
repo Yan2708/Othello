c7616ac05e738480beb96256e3ef1822a7bf75e6
Yan2708 <74718439+Yan2708@users.noreply.github.com>
2023-04-29 13:54:16 +0200

    clean

--- AI/AI_Player.py
+++ AI/AI_Player.py
@@ -3,90 +3,89 @@ from math import inf
 import copy
 import sys
 from GUI.utils import Utils
-#TODO: Set strategie
+
+
+# TODO: Set strategie
 class Strategy():
-    def __init__(self,player_max,depth_max,weighting_nb_stroke,weighting_nb_pawns,weighting_position):
+
+    def __init__(self, player_max, depth_max, weighting_nb_stroke, weighting_nb_pawns, weighting_position):
         self.player_max = player_max
         self.depth_max = depth_max
         self.weighting_nb_pawns = weighting_nb_pawns
         self.weighting_nb_stroke = weighting_nb_stroke
         self.weighting_position = weighting_position
-    
-    
+
     def set_weighting_nb_pawns(self, value):
         self.weighting_nb_pawns = value
-    
+
     def set_weighting_nb_stroke(self, value):
         self.weighting_nb_stroke = value
-    
+
     def set_weighting_position(self, value):
         self.weighting_position = value
-    
+
     @staticmethod
-    def evaluate_nb_pawns(board,player):
-        return board.get_pawns_nb(player) - board.get_pawns_nb(not player )
-    
+    def evaluate_nb_pawns(board, player):
+        return board.get_pawns_nb(player) - board.get_pawns_nb(not player)
+
     @staticmethod
-    def evaluate_nb_strokes(board,player):
+    def evaluate_nb_strokes(board, player):
         return len(Rules.movespossible(board, player)) - len(Rules.movespossible(board, not player))
-    
+
     @staticmethod
-    def evaluate_strenght_position(board,player):
+    def evaluate_strenght_position(board, player):
         Sum_strenght = 0
         for x in range(8):
             for y in range(8):
-                current = (x,y)
+                current = (x, y)
                 if Rules.is_in_corner(current):
-                    Sum_strenght = Sum_strenght+10 if board.status[x][y] == player else Sum_strenght-10
+                    Sum_strenght += 10 if board.status[x][y] == player else -10
                 elif Rules.checkadjacent_for_corner(current):
-                    Sum_strenght = Sum_strenght-5 if board.status[x][y] == player else Sum_strenght+5
+                    Sum_strenght += -5 if board.status[x][y] == player else 5
                 elif Rules.is_in_border(current):
-                    Sum_strenght = Sum_strenght+3 if board.status[x][y] == player else Sum_strenght-3
+                    Sum_strenght += 3 if board.status[x][y] == player else -3
                 else:
-                    Sum_strenght = Sum_strenght+1 if board.status[x][y] == player else Sum_strenght-1
+                    Sum_strenght += 1 if board.status[x][y] == player else -1
         return Sum_strenght
-    
 
-    def global_evaluate(self,board,player):
-        return Strategy.evaluate_nb_strokes(board,player)*self.weighting_nb_stroke+Strategy.evaluate_nb_pawns(board, player)*self.weighting_nb_pawns+Strategy.evaluate_strenght_position(board, player)*self.weighting_position
-    
-    @Utils.timer
-    def alpha_beta_search(self,board,alpha,beta,current_player,depth):
-        print(depth)
-        if depth == 0 or board.isfull() : 
-            return self.global_evaluate(board,current_player)
-        
-        m = -float('inf') if current_player == self.player_max else float('inf')
+    def global_evaluate(self, board, player):
+        return Strategy.evaluate_nb_strokes(board, player) * self.weighting_nb_stroke + \
+            Strategy.evaluate_nb_pawns(board, player) * self.weighting_nb_pawns + \
+            Strategy.evaluate_strenght_position(board, player) * self.weighting_position
+
+    def alpha_beta_search(self, board, alpha, beta, current_player, depth):
+        if depth == 0 or board.isfull():
+            return self.global_evaluate(board, current_player)
+
+        m = -inf if current_player == self.player_max else inf
         update_alpha_beta = max if current_player == self.player_max else min
 
-        for move in Rules.movespossible(board,current_player):
-            board.simulate_stroke(move,current_player)
-            val = self.alpha_beta_search(board,alpha,beta,not current_player,depth-1)
+        for move in Rules.movespossible(board, current_player):
+            board.simulate_stroke(move, current_player)
+            val = self.alpha_beta_search(board, alpha, beta, not current_player, depth - 1)
             board.undo_move(move)
             m = update_alpha_beta(m, val)
-            
-            if current_player == self.player_max: 
-                alpha = max(alpha, m) 
-                
+
+            if current_player == self.player_max:
+                alpha = max(alpha, m)
+
             else:
-                beta = min(beta, m)   
-                
+                beta = min(beta, m)
+
             if beta <= alpha:
                 break
         return m
-    
+
     @Utils.timer
-    def get_best_move(self,state):
+    def get_best_move(self, state):
         best_score = -inf
         best_move = None
         fake_state = copy.deepcopy(state)
-        for pos in Rules.movespossible(fake_state,self.player_max):
-            fake_state.simulate_stroke(pos,self.player_max)
+        for pos in Rules.movespossible(fake_state, self.player_max):
+            fake_state.simulate_stroke(pos, self.player_max)
             score = self.alpha_beta_search(fake_state, -inf, inf, self.player_max, self.depth_max)
-            print(pos,score)
             fake_state.undo_move(pos)
-            if score > best_score or (best_move is None and (score == -inf or score == inf)) :
+            if score > best_score or (best_move is None and (score == -inf or score == inf)):
                 best_score = score
                 best_move = pos
         return best_move
-        
--- Game/Rules.py
+++ Game/Rules.py
@@ -7,12 +7,13 @@ Created on Sun Apr  2 15:58:06 2023
 from Game.Board import Board
 import numpy as np
 
+
 def isplayable(board, x, y, p):
     return board.status[x][y] is None and checkadjacent(board, x, y, not p) and Board.flips(board, x, y, p)
 
 
 def checkadjacent(board, x, y, p):
-    positions_adjacentes = np.array([(-1, -1),  (-1, 0),  (-1, 1),  (0, -1),  (0, 1),  (1, -1),  (1, 0),  (1, 1)])
+    positions_adjacentes = np.array([(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)])
     for i, j in positions_adjacentes:
         try:
             if board.status[x + i][y + j] == p:
@@ -21,7 +22,8 @@ def checkadjacent(board, x, y, p):
             pass
     return False
 
-#TODO:Mettre dans board ?
+
+# TODO:Mettre dans board ?
 def movespossible(board, p):
     t = []
     for i in range(8):
@@ -30,11 +32,14 @@ def movespossible(board, p):
                 t.append((i, j))
     return t
 
+
 def is_in_corner(position):
-    corner =  np.array([(0,0),(0,7),(7,7),(7,0)])
+    corner = np.array([(0, 0), (0, 7), (7, 7), (7, 0)])
     if position in corner:
         return True
     return False
+
+
 def is_in_border(position):
     if is_in_corner(position) == False:
         if position[0] == 0 or position[0] == 7:
@@ -43,14 +48,13 @@ def is_in_border(position):
             return True
     return False
 
+
 def checkadjacent_for_corner(p):
-    corner = np.array([(0,0),(0,7),(7,7),(7,0)])
+    corner = np.array([(0, 0), (0, 7), (7, 7), (7, 0)])
     is_adj = False
     for t in corner:
-        if t[0] == p[0] and abs(t[1]-p[1]) == 1 or t[1] == p[1] and abs(t[0]-p[0]) == 1:
+        if t[0] == p[0] and abs(t[1] - p[1]) == 1 or t[1] == p[1] and abs(t[0] - p[0]) == 1:
             is_adj == True
         if is_adj:
             break
     return is_adj
-    
-
